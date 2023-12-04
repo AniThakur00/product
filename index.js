eee22df903d043e08d68f275fe6df11997a3cc27
Tanay Van <tanayvan@Tanays-MacBook-Pro.local>
2023-12-03 19:49:07 -0500

    added real code

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const http = require('http');
+require('dotenv').config()
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from product. TESTING 7. container port - 3000, service port - 80.\n');
-});
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
-const port = 3000;
-server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+
+const app = express()
+//My Routes
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
+const productRoutes = require('./routes/product')
+const orderRoutes = require('./routes/order')
+const stripeRoute = require('./routes/stripePayment')
+//MiddleWares
+app.use(bodyParser.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+//Database Connection
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => {
+    console.log("Database Connected")
+  }).catch(() => {
+    console.log("Error Connecting to Database")
+  })
+
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: "Woah product is working"
+  })
+})
+
+//My Routes
+app.use('/api', authRoutes)
+app.use('/api', userRoutes)
+app.use('/api', categoryRoutes)
+app.use('/api', productRoutes)
+app.use('/api', orderRoutes)
+app.use('/api', stripeRoute)
+
+
+
+//Port Number and Starting The Server
+const port = 3000 || process.env.PORT
+app.listen(port, () => {
+  console.log(`Server Started on ${port}`)
+})
